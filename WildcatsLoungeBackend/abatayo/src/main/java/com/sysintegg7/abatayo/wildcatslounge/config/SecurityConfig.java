c983356a4c4738b445ce205557b2622fb943477c
Archienni Al R. Abatayo <122308950+Archinix@users.noreply.github.com>
2026-05-09 00:52:03 +0800

    Fix global CORS configuration in SecurityConfig and WebSocketConfig

--- WildcatsLoungeBackend/abatayo/src/main/java/com/sysintegg7/abatayo/wildcatslounge/config/SecurityConfig.java
+++ WildcatsLoungeBackend/abatayo/src/main/java/com/sysintegg7/abatayo/wildcatslounge/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://csit-342-g7-wildcats-lounge-app.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
