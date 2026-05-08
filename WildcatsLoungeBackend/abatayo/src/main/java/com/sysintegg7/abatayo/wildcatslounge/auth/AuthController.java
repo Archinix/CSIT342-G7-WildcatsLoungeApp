a892f40d4c97bc86c49c44148854909cf8631f6d
Archienni Al R. Abatayo <122308950+Archinix@users.noreply.github.com>
2026-05-08 18:28:51 +0800

    Configure CORS for production Vercel frontend URL

--- WildcatsLoungeBackend/abatayo/src/main/java/com/sysintegg7/abatayo/wildcatslounge/auth/AuthController.java
+++ WildcatsLoungeBackend/abatayo/src/main/java/com/sysintegg7/abatayo/wildcatslounge/auth/AuthController.java
@@ -1,10 +1,17 @@
 package com.sysintegg7.abatayo.wildcatslounge.auth;
 
-import com.sysintegg7.abatayo.wildcatslounge.RegistrationPage.RegisterRepository;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sysintegg7.abatayo.wildcatslounge.RegistrationPage.RegisterRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
