f805764bd94dc07348dd0571787b91878c80930f
Archienni Al R. Abatayo <122308950+Archinix@users.noreply.github.com>
2026-05-09 15:20:45 +0800

    Make email sending async to prevent SMTP timeouts from blocking password reset

--- WildcatsLoungeBackend/abatayo/src/main/java/com/sysintegg7/abatayo/wildcatslounge/AbatayoApplication.java
+++ WildcatsLoungeBackend/abatayo/src/main/java/com/sysintegg7/abatayo/wildcatslounge/AbatayoApplication.java
@@ -2,8 +2,10 @@ package com.sysintegg7.abatayo.wildcatslounge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class AbatayoApplication {
 
 	public static void main(String[] args) {
--- WildcatsLoungeBackend/abatayo/src/main/java/com/sysintegg7/abatayo/wildcatslounge/auth/PasswordResetService.java
+++ WildcatsLoungeBackend/abatayo/src/main/java/com/sysintegg7/abatayo/wildcatslounge/auth/PasswordResetService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.sysintegg7.abatayo.wildcatslounge.RegistrationPage.RegisterEntity;
@@ -112,6 +113,7 @@ public class PasswordResetService {
         return !resetToken.getUsed() && !resetToken.isExpired();
     }
 
+    @Async
     private void sendEmail(String email, String firstName, String resetLink) {
         try {
             logger.info("Attempting to send password reset email to: {}", email);
